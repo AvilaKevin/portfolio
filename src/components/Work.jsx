c7bb7f2167027faaf65e31fcfe181f098b523593
ViperDex <viperdex2000@hotmail.com>
2023-05-24 11:42:45 -0500

    some projects added to work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import AppMernStack from '../assets/work/AppMernStack.png';
+import YourVideos from '../assets/work/YourVideos.png';
 import weatherApp from '../assets/work/WeatherApp.png';
 import QrAccess from '../assets/work/QrAccess.png';
 import ToDo from '../assets/work/ToDo.png';
@@ -20,15 +22,47 @@ const Work = () => {
                 {/* GRID ITEMS */}
                 <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
 
-                    {/* GRID weatherApp */}
-                    <div style={{ background: `url(${weatherApp})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    {/* GRID YouTubeClone */}
+                    <div style={{ background: `url(${YourVideos})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                         {/* Hover Effects */}
                         <div className='opacity-0 group-hover:opacity-100'>
                             <span className='text-2xl font-bold text-white tracking-wider'>
-                                React JS Application
+                                YouTubeClone
                             </span>
                             <div className='pt8 text-center'>
+                                <a href='https://github.com/AvilaKevin/YouTubeClone'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* GRID AppMernStack */}
+                    <div style={{ background: `url(${AppMernStack})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                AppMernStack
+                            </span>
+                            <div className='pt8 text-center'>
+                                <a href='https://github.com/AvilaKevin/CrudMernStack'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* GRID weatherApp */}
+                    <div style={{ background: `url(${weatherApp})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                        {/* Hover Effects */}
+                        <div className='flex flex-col items-center opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                weatherApp
+                            </span>
+                            <div className='pt8'>
                                 <a href='https://avilakevin.github.io/wheather_app/'>
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                                 </a>
@@ -44,11 +78,11 @@ const Work = () => {
                     <div style={{ background: `url(${QrAccess})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                         {/* Hover Effects */}
-                        <div className='opacity-0 group-hover:opacity-100'>
+                        <div className='flex flex-col items-center opacity-0 group-hover:opacity-100'>
                             <span className='text-2xl font-bold text-white tracking-wider'>
-                                React JS Application
+                                QrAccess
                             </span>
-                            <div className='pt8 text-center'>
+                            <div className='pt8'>
                                 <a href='https://github.com/AvilaKevin/qr-access-validator'>
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                                 </a>
@@ -60,11 +94,11 @@ const Work = () => {
                     <div style={{ background: `url(${ToDo})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                         {/* Hover Effects */}
-                        <div className='opacity-0 group-hover:opacity-100'>
+                        <div className='flex flex-col items-center opacity-0 group-hover:opacity-100'>
                             <span className='text-2xl font-bold text-white tracking-wider'>
-                                React JS Application
+                                ToDo
                             </span>
-                            <div className='pt8 text-center'>
+                            <div className='pt8'>
                                 <a href='https://avilakevin.github.io/To-Do-List/'>
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                                 </a>
@@ -79,11 +113,11 @@ const Work = () => {
                     <div style={{ background: `url(${MathTools})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                         {/* Hover Effects */}
-                        <div className='opacity-0 group-hover:opacity-100'>
+                        <div className='flex flex-col items-center opacity-0 group-hover:opacity-100'>
                             <span className='text-2xl font-bold text-white tracking-wider'>
-                                React JS Application
+                                MathTools
                             </span>
-                            <div className='pt8 text-center'>
+                            <div className='pt8'>
                                 <a href='https://avilakevin.github.io/Platzi-Curso-practico-js/'>
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                                 </a>
